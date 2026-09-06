e744b4d719ea577fb0914a895dddfe7e0b1d5079
juanagekdyszman <juanagekdyszman10@gmail.com>
2026-09-06 18:11:56 -0300

    Add files via upload

---
+++ for.php
@@ -0,0 +1,21 @@
+<?php
+
+//ejemplo 1
+for($B=1; $B<=20; $B++){
+    echo $B."<br>";
+}
+for($B=20; $B>=1; $B--){
+echo $B."<br>";
+}
+
+//ejemplo 2
+
+$num=7;
+for($C=1; $C<=12; $C++){
+echo $num." X ".$C. " = ".$C*$num."<br>";
+}
+
+$num=7;
+for($C=12; $C>=1; $C--){
+echo $num." X ".$C. " = ".$C*$num."<br>";
+}
