437bb23f8f24d9522b119bb3e448bf9ed56c84e0
juanagekdyszman <juanagekdyszman10@gmail.com>
2026-09-10 12:52:03 -0300

    Add files via upload

---
+++ forfereach.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero=7;
+
+for($i=1; $i<=20; $i++){
+    echo $i. "<br>";
+
+}
+
+for($i=20; $i>=1; $i--){
+    echo  $i. "<br>";
+
+}
+
+for($i=1; $i<=12; $i++){
+    echo  $i*$numero." x ".  $i <br>;
+
+}
